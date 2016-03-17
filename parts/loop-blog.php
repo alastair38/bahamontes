2307a1cbd07c8f01f576a1f78581f13097da1f2e
alastair38 <alastair@alastaircox.com>
2016-03-17 21:09:19 +0000

    Version 1.2: correct home background image path, add favicon and mobile tile images. Tweaked parallax image size for mobile to see if clearer

--- parts/loop-blog.php
+++ parts/loop-blog.php
@@ -1,13 +1,14 @@
 <div class="parallax-container">
-    <div class="parallax"><?php the_post_thumbnail('full');?></div>
+    <div class="parallax hide-on-small-only"><?php the_post_thumbnail('full');?></div>
+    <div class="parallax hide-on-med-and-up"><?php the_post_thumbnail('large');?></div>
 </div>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('container'); ?> role="article">
-	<section class="">
+	<section>
 		<h2><a href="<?php the_permalink() ?>" class="center" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 			<?php get_template_part( 'parts/content', 'byline' ); ?>
 		<?php the_excerpt(); ?>
 	</section>
-	
+
 
 </article>
